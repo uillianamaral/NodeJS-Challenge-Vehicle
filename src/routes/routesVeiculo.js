007076e46946b8fb8958075e76e3ec95a93e4415
uillianamaral <uillianhenrique@hotmail.com>
2019-09-29 15:08:43 -0300

    add update, findOne, findAll e algumas validações

--- src/routes/routesVeiculo.js
+++ src/routes/routesVeiculo.js
@@ -1,29 +1,33 @@
 module.exports = (app, db) => {
     // BUSCAR TODOS OS VEICULOS
-    app.get('/veiculo', (req, res) => {
-      db.veiculo.findAll()
+    app.get('/veiculo', async (req, res) => {
+      await db.veiculo.findAll()
         .then(veiculo => {
           res.status(200).json(veiculo);
         });
     });
   
     // BUSCAR APENAS UM VEICULO POR ID
-    app.get('/veiculo/:id', (req, res) => {
-      const id = req.params.id;
-      db.veiculo.find({
-        where: { id: id}
-      })
-        .then(veiculo => {
+    app.get('/veiculo/:id', async (req, res) => {
+      const idVeiculo = req.params.id;
+     await db.veiculo.findOne({
+        where: { id: idVeiculo}
+      }).then(veiculo => {
+          if(!veiculo) {
+            return res.status(404).json({"Id invalido: " : veiculo});
+          }
           res.status(200).json(veiculo);
+        }).catch(err => {
+          res.status(400).json({"Erro: " : err})
         });
     });
   
     // CRIAR UM VEICULO
-    app.post('/veiculo', (req, res) => {
+    app.post('/veiculo', async (req, res) => {
       
       const {nomeVeiculo, marca, ano, descricao, vendido} = req.body;
 
-      db.veiculo.create({
+      await db.veiculo.create({
         nomeVeiculo: nomeVeiculo,
         marca: marca,
         ano: ano,
@@ -32,33 +36,37 @@ module.exports = (app, db) => {
       })
         .then(createVeiculo => {
         res.status(201).json(createVeiculo);
+      }).catch(err => {
+          res.status(404).json({"Erro ao cadastrar o Veiculo " : err});
       });
     });
   
     // ATUALIZAR UM VEICULO PASSANDO O ID
-    app.patch('/veiculo/:id', (req, res) => {
+     app.put('/veiculo/:id', async (req, res) => {
       const id = req.params.id;
-      const updates = req.body.updates;
-      db.veiculo.find({
+      const updates = {nomeVeiculo, marca, descricao, ano, vendido} = req.body;
+      await db.veiculo.findOne({
         where: { id: id }
-      })
-        .then(veiculo => {
-          return veiculo.updateAttributes(updates);
+      }).then(veiculo => {
+          return veiculo.update(updates);
         })
         .then(updatedVeiculo => {
           res.status(200).json(updatedVeiculo);
-        }).catch(err => {
-          res.status(400).json({"message": err})
+        }).catch(() => {
+          res.status(404).json({"message": "ID inexistente"})
         });
     });
   
     // DELETAR UM VEICULO POR ID
-    app.delete('/veiculo/:id', (req, res) => {
-      const id = req.params.id;
-      db.veiculo.destroy({
-        where: { id: id }
+    app.delete('/veiculo/:id', async (req, res) => {
+      const idVeiculo = req.params.id;
+      await db.veiculo.destroy({
+        where: { id: idVeiculo}
       }).then(deletedVeiculo => {
-          res.status(200).json({"id":deletedVeiculo, "message": "Excluido com sucesso!!"});
+        if(!deletedVeiculo){
+          return res.status(404).json({"message": "ID inexistente"});
+        }
+        res.status(200).json({"message": "Veiculo excluido com sucesso!!"});
         }).catch(err =>{
           res.status(401).json({"message": "Erro ao excluir!!", err});
         });
